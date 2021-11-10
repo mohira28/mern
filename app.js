7f9e6a2d644255c304a247e94f9124a472a2130e
mohira28 <90238924+mohira28@users.noreply.github.com>
2021-11-10 14:37:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const config = require('config')
 const path = require('path')
 const mongoose = require('mongoose')
-//copied
+
 const app = express()
 
 app.use(express.json({ extended: true }))
